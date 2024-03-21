87333d002ceed87e6dc730f4cb832d1a19b34d02
Isaq <146621374+IssaZ11@users.noreply.github.com>
2024-03-21 10:16:48 +0100

    Add files via upload

---
+++ build/models/saved-interval.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SavedInterval = void 0;
+var SavedInterval = /** @class */ (function () {
+    function SavedInterval(setcode, channel, interval) {
+        this.setcode = setcode;
+        this.channel = channel;
+        this.interval = interval;
+    }
+    return SavedInterval;
+}());
+exports.SavedInterval = SavedInterval;
---
+++ build/models/watched-setcode.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WatchedSetCode = void 0;
+var WatchedSetCode = /** @class */ (function () {
+    function WatchedSetCode(setCode, channelID) {
+        this.setCode = setCode;
+        this.channelID = channelID;
+    }
+    return WatchedSetCode;
+}());
+exports.WatchedSetCode = WatchedSetCode;
